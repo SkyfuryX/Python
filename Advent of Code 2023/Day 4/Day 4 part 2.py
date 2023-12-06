5d9b0b9f7fa8e83c571034b4435ec1b65efea1ed
Gannon S <121438046+SkyfuryX@users.noreply.github.com>
2023-12-05 20:49:59 -0500

    Update Day 4 part 2.py
    Cleaned up the code

--- Advent of Code 2023/Day 4/Day 4 part 2.py
+++ Advent of Code 2023/Day 4/Day 4 part 2.py
@@ -5,18 +5,14 @@ import re
 data = open('day4input.txt').read().strip()
 score = 0
 current_card = 0
-next_card = 0
-next_card2 = 0
-next_card3 = 0
-next_card4 = 0
-next_card5 = 0
-next_card6 = 0
-next_card7 = 0
-next_card8 = 0
-next_card9 = 0
-next_card10 = 0
+global next_card
+next_card = [0,0,0,0,0,0,0,0,0,0,0,0]
 
-for game, line in enumerate(data.split("\n"), start=1):                       
+def copies(number_matching):
+    for n in range(number_matching):
+        next_card[n+1] += 1    
+
+for line in data.split("\n"):                       
     #setup    
     score += 1
     numbers = line.split(": ")[-1].split("| ")
@@ -30,63 +26,19 @@ for game, line in enumerate(data.split("\n"), start=1):
                 number_matching += 1
 
     #adds copies of cards to come        
-    if number_matching >= 1:
-        next_card += 1
-    if number_matching >= 2:
-        next_card2 += 1
-    if number_matching >= 3:
-        next_card3 += 1
-    if number_matching >= 4:
-        next_card4 += 1
-    if number_matching >= 5:
-        next_card5 += 1
-    if number_matching >= 6:
-        next_card6 += 1
-    if number_matching >= 7:
-        next_card7 += 1
-    if number_matching >= 8:
-        next_card8 += 1
-    if number_matching >= 9:
-        next_card9 += 1
-    if number_matching == 10:
-        next_card10 += 1
+    copies(number_matching)
 
     #add copies to score and increments further
-    if current_card >=1:
-        score += current_card
-        for copy in range(current_card):
-            if number_matching >= 1:
-                next_card += 1
-            if number_matching >= 2:
-                next_card2 += 1
-            if number_matching >= 3:
-                next_card3 += 1
-            if number_matching >= 4:
-                next_card4 += 1
-            if number_matching >= 5:
-                next_card5 += 1
-            if number_matching >= 6:
-                next_card6 += 1
-            if number_matching >= 7:
-                next_card7 += 1
-            if number_matching >= 8:
-                next_card8 += 1
-            if number_matching >= 9:
-                next_card9 += 1
-            if number_matching == 10:
-                next_card10 += 1   
+    if next_card[0] >=1:
+        score += next_card[0]
+        for copy in range(next_card[0]):
+            copies(number_matching)  
 
     #moves the number of copies to come up the chain and resets the farthest one out
-    current_card = next_card
-    next_card = next_card2
-    next_card2 = next_card3
-    next_card3 = next_card4
-    next_card4 = next_card5
-    next_card5 = next_card6
-    next_card6 = next_card7
-    next_card7 = next_card8
-    next_card8 = next_card9
-    next_card9 = next_card10
-    next_card10 = 0
+    for n in range(len(next_card)):
+        try:
+            next_card[n] = next_card[n+1]
+        except:
+            IndexError
 
 print('The score is: ' + str(score))
